e751ddac55a36cb5849e12922b7972ea367f0921
sunitbelbase <sunit@sunits-MacBook-Pro.local>
2015-12-17 10:28:43 +1100

    17/2015 by sunit
    FTP class added

--- src/com/book/controller/FileUploadServlet.java
+++ src/com/book/controller/FileUploadServlet.java
@@ -1,7 +1,9 @@
 package com.book.controller;
+//package com.book.service;
 import java.io.File;
 import java.io.IOException;
-  
+import java.net.URLConnection;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -9,18 +11,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+
   
 @WebServlet("/FileUploadServlet")
 @MultipartConfig(fileSizeThreshold=1024*1024*10,    // 10 MB 
                  maxFileSize=1024*1024*50,          // 50 MB
                  maxRequestSize=1024*1024*100)      // 100 MB
 public class FileUploadServlet extends HttpServlet {
-  
    
      
     /**
 	 *  private static final long serialVersionUID = 205242440643911308L;
 	 */
+	private URLConnection m_client;
 	private static final long serialVersionUID = 1L;
 	/**
      * Directory where uploaded files will be saved, its relative to
@@ -31,6 +34,7 @@ public class FileUploadServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
         // gets absolute path of the web application
+    	SimpleFTPClient da;
         String applicationPath = request.getServletContext().getRealPath("fileName");
         // constructs path of the directory to save uploaded file
         String uploadFilePath = applicationPath + File.separator + UPLOAD_DIR;
@@ -57,6 +61,10 @@ public class FileUploadServlet extends HttpServlet {
     /**
      * Utility method to get file name from HTTP header content-disposition
      */
+    private void UploadFile()
+    {
+    	
+    }
     private String getFileName(Part part) {
         String contentDisp = part.getHeader("content-disposition");
         System.out.println("content-disposition header= "+contentDisp);
